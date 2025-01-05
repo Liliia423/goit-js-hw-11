e9b81bbdcf1c07ffd9e2c55349d1222370df8498
Liliia423 <etude4408@gmail.com>
2025-01-05 21:27:10 +0200

    tyu

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,32 +5,37 @@ const submitBtn = document.querySelector('.submit-button');
 const searchField = document.querySelector('.search-field');
 const searchForm = document.querySelector('.search-image-form');
 
-searchForm.addEventListener('keydown', event => {
+/*======== заборона відправки форми натисканням Enter ========*/
+
+/*searchForm.addEventListener('keydown', event => {
   if (event.key === 'Enter') {
     event.preventDefault();
     iziToast.warning({
       message: 'Don`t use key! Use Search button!',
     });
   }
-});
+});*/
 
 submitBtn.addEventListener('click', () => {
   event.preventDefault();
-  const query = searchField.value.trim();
 
-  if (!query) {
+  /*==== помилка порожнє поле ====*/
+  const query = searchField.value.trim();
+  /*if (!query) {
     iziToast.error({
       message: `Sorry, there are no images matching your search query. Please, try again!`,
       position: 'topRight',
       class: 'error-toast',
-      /*timeout: 400000,*/
+      timeout: 400000,
       closeOnClick: true,
     });
-    return; // Зупиняємо виконання функції, якщо поле порожнє
-  }
+    return;
+  }*/
 
   fetch(
-    `https://pixabay.com/api/?key=45378122-3aa1f0accb7d59cfaae2c348a&q=query&image_type=photo&orientation=horizontal&safesearch=true`
+    `https://pixabay.com/api/?key=45378122-3aa1f0accb7d59cfaae2c348a&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+
+    /*`https://pixabay.com/api/?key=45378122-3aa1f0accb7d59cfaae2c348a&q=query&image_type=photo&orientation=horizontal&safesearch=true`*/
   )
     .then(response => {
       if (!response.ok) {
@@ -39,17 +44,17 @@ submitBtn.addEventListener('click', () => {
       return response.json();
     })
     .then(data => {
-      if (data.hits.length === 0) {
-        // Перевірка, чи масив порожній
+      // Перевірка, чи масив порожній
+      /*if (data.hits.length === 0) {
         iziToast.error({
           message: `Sorry, there are no images matching your search query. Please, try again!`,
           position: 'topRight',
           class: 'error-toast',
-          /*timeout: 400000,*/
+          timeout: 400000,
           closeOnClick: true,
         });
         return;
-      }
+      }*/
 
       console.log(data.hits);
     })
@@ -58,7 +63,7 @@ submitBtn.addEventListener('click', () => {
         message: `Sorry, there are no images matching your search query. Please, try again!`,
         position: 'topRight',
         class: 'error-toast',
-        /*timeout: 400000,*/
+        timeout: 400000,
         closeOnClick: true,
       });
     });

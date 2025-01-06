11a7bf81c8b0fdfa68a97c47dcdfa90aec4c6d13
Liliia423 <etude4408@gmail.com>
2025-01-06 12:13:04 +0200

    rty

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -15,3 +15,27 @@
         `;
         gallery.insertAdjacentHTML('beforeend', imageHTML);
       });*/
+
+const tegMain = document.querySelector('main');
+
+// Створюємо форму
+const tegForm = document.createElement('form');
+tegForm.className = 'search-image-form';
+tegMain.appendChild(tegForm); // Додаємо форму в main
+
+// Створюємо input
+const tegInput = document.createElement('input');
+tegInput.type = 'text';
+tegInput.name = 'field';
+tegInput.className = 'search-field';
+tegInput.placeholder = 'Search images...';
+tegForm.appendChild(tegInput); // Додаємо input у форму
+
+// Створюємо кнопку
+const tegButton = document.createElement('button');
+tegButton.textContent = 'Search';
+tegButton.type = 'submit';
+tegButton.className = 'submit-button';
+tegForm.appendChild(tegButton); // Додаємо кнопку у форму
+
+tegMain.prepend(tegForm);
